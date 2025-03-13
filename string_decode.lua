b52621691e6668e328171e9d91fc084ed0469c16
Petr Pucil <petr.pucil@seznam.cz>
2025-03-13 10:58:29 +0100

    stringdecode.decode(): fix Lua 5.4 compatibility
    The `bit32` library is not present in Lua 5.4, so we got the following
    error whenever a UTF-8 string was decoded
    (https://github.com/kaitai-io/ci_artifacts/blob/c167cf7e1743495342021231f1fc5ee39c893da7/test_out/lua/ci.json#L243):
    
    ```
    ../runtime/lua/string_decode.lua:19: attempt to index a nil value (global 'bit32')
    ```
    
    The fix is to use the bitwise operators available since Lua 5.3. Their
    use leads to a syntax error in Lua 5.2 and earlier, but we already use
    them in `kaitaistruct.lua`, so we don't break existing compatibility.

--- string_decode.lua
+++ string_decode.lua
@@ -16,9 +16,9 @@ local function utf8_to_32(utf8str)
             seq = c < 0x80 and 1 or c < 0xE0 and 2 or c < 0xF0 and 3 or
                   c < 0xF8 and 4 or --c < 0xFC and 5 or c < 0xFE and 6 or
                 error("Invalid UTF-8 character sequence")
-            val = bit32.band(c, 2^(8-seq) - 1)
+            val = c & (2^(8-seq) - 1)
         else
-            val = bit32.bor(bit32.lshift(val, 6), bit32.band(c, 0x3F))
+            val = (val << 6) | (c & 0x3F)
         end
 
         seq = seq - 1
